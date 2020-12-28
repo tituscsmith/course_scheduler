b947cba8f2eebd0d96a09e0b4c700fb9130e6682
tituscsmith <tsmith42@wisc.edu>
2020-12-27 20:37:15 -0500

    Updates

---
+++ precache-manifest.5b61c4c97592594253509220c7a122f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ee650ecfb61ad7fde830e5778c3f36",
+    "url": "/course_scheduler/index.html"
+  },
+  {
+    "revision": "f072b518efe638cdc1fb",
+    "url": "/course_scheduler/static/css/main.7ab43222.chunk.css"
+  },
+  {
+    "revision": "4420f967f902c3846349",
+    "url": "/course_scheduler/static/js/2.7695151f.chunk.js"
+  },
+  {
+    "revision": "f072b518efe638cdc1fb",
+    "url": "/course_scheduler/static/js/main.8590674b.chunk.js"
+  },
+  {
+    "revision": "045921d0f45481f93ebf",
+    "url": "/course_scheduler/static/js/runtime~main.383e0d94.js"
+  },
+  {
+    "revision": "6f1896d9395b2868078d9ba813339adf",
+    "url": "/course_scheduler/static/media/big-classroom.6f1896d9.jpg"
+  },
+  {
+    "revision": "c84f065c6daeab1e900c6d520d65601c",
+    "url": "/course_scheduler/static/media/cloud.c84f065c.jpg"
+  },
+  {
+    "revision": "a2045c22e83b02f77e8ca79a13f0313b",
+    "url": "/course_scheduler/static/media/lady-recommendation.a2045c22.jpg"
+  },
+  {
+    "revision": "e691947a01126a2c5c1188378bd7cb81",
+    "url": "/course_scheduler/static/media/race-finish.e691947a.jpg"
+  }
+]);
